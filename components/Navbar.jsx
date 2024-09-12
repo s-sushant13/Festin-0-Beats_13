aa92010246ef6f9d1a011fc05e824609b64dfe3c
prateek tiwari <tiwariprateek1976@gmail.com>
2024-09-12 23:54:20 +0530

    register

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -154,7 +154,7 @@ const Navbar = () => {
                             <button className="relative inline-flex h-8 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#FFA357_0%,#FFA357_50%,#65B4D4_100%)]" />
                                 <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-                                    Sponsor Us
+                                    Register Now
                                 </span>
                             </button>
                         </Link>
