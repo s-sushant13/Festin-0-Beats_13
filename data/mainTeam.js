c8d8eb84dde424fcd7800b7e2d575f6e42cd10c2
prateek tiwari <tiwariprateek1976@gmail.com>
2024-09-16 12:15:30 +0530

    changes

--- data/mainTeam.js
+++ data/mainTeam.js
@@ -5,11 +5,7 @@ export const mainTeam = [
         img: "https://media.licdn.com/dms/image/v2/D5603AQHE_pu5UZmgwA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1726464155619?e=1732147200&v=beta&t=rlUxJ5txotVPiZPw3fB-uPBpRi6pgfkrT6kjXTvkLgE",
         linkedin: "https://www.linkedin.com/in/tanishqsharma01/",
     },
-    {
-        name: "Prabhuti Vashisth ",
-        img: "https://media.licdn.com/dms/image/v2/D5603AQEky8xWobN7Cw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1726144939911?e=1731542400&v=beta&t=srrhoSMeLwdZ5D7OvMUip0HI4wlessvnNN5cXrBllV8",
-        linkedin: "https://www.linkedin.com/in/prabhuti-vashisth-898598300/",
-    },
+    
     {
         name: "Arjun Dhawan",
         img: "https://media.licdn.com/dms/image/v2/D4D03AQGGz_HUQgyPDg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1693031039739?e=1731542400&v=beta&t=QcSAjunj_ZTL6aX7fP3CM_H55r2fQzqqM3O_cgyeuDg",
@@ -20,6 +16,12 @@ export const mainTeam = [
         name: "Siddhant Srivastava",
         img: "https://media.licdn.com/dms/image/v2/D4D03AQEkJM5nOMhohg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1675002177682?e=1731542400&v=beta&t=kxri-JkI5WRxWsAIajV3vUzMq3Qps4lZ09sS4Kovj3g",
         linkedin: "https://www.linkedin.com/in/siddhantsrivastava2911/",
+    },
+
+    {
+        name: "Prabhuti Vashisth ",
+        img: "https://media.licdn.com/dms/image/v2/D5603AQEky8xWobN7Cw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1726144939911?e=1731542400&v=beta&t=srrhoSMeLwdZ5D7OvMUip0HI4wlessvnNN5cXrBllV8",
+        linkedin: "https://www.linkedin.com/in/prabhuti-vashisth-898598300/",
     },
     
     {
